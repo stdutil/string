a3769eddb5cffbc72094af427891b40514e5098a
Elizalde Baguinon <mizmerize@yahoo.com>
2024-11-18 15:38:06 +0800

    Removed methods

--- string.go
+++ string.go
@@ -2,28 +2,6 @@ package string
 
 import "strings"
 
-type String struct{}
-
-// StripEndingForwardSlash removes the ending forward slash of a string
-func (s *String) StripEndingForwardSlash(value string) string {
-	return StripEndingForwardSlash(value)
-}
-
-// StripLeading strips string of leading characters by an offset
-func (s *String) StripLeading(value string, offset int) string {
-	return StripLeading(value, offset)
-}
-
-// StripTrailing strips string of trailing characters after the length
-func (s *String) StripTrailing(value string, length int) string {
-	return StripTrailing(value, length)
-}
-
-// ToByte converts string or strings to byte array with an option for a separator
-func (s *String) ToByte(sep string, elems ...string) []byte {
-	return StringToByte(sep, elems...)
-}
-
 // StringToByte converts string or strings to byte array with an option for a separator
 func StringToByte(sep string, elems ...string) []byte {
 	return []byte(strings.Join(elems, sep))
